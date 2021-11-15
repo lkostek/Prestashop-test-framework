1c71fc62353e283b1d165a29d4800bdddcfc1995
Łukasz Kostek <lkostek@users.noreply.github.com>
2021-11-15 16:02:41 +0100

    getElement method created;

--- base/core.py
+++ base/core.py
@@ -14,7 +14,7 @@ class Core:
     log = logger.loggerInstance()
 
     def __init__(self, driver):
-        self.driver = driver
+        self.driver = webdriver.Firefox() #TODO zmienic na driver
 
     def getTypeOfLocator(self, type):
         """Zwraca element By.X w zaleznosci od podanego parametru."""
@@ -33,5 +33,20 @@ class Core:
             self.log.error(f"Locator's type '{type}' is not supported")
             traceback.print_stack()
         return False
+
+    def getElement(self, type_of_locator, locator):
+        """Zwraca znaleziony element po podanych parametrach na stronie."""
+
+        try:
+            by_type = self.getTypeOfLocator(type_of_locator)
+            element = self.driver.find_element(by_type, locator)
+            self.log.info(
+                f'FOUND element with locator type: '
+                f'{type_of_locator} and locator: {locator}')
+        except Exception:
+            self.log.error(
+                f'NOT FOUND element with locator type: '
+                f'{type_of_locator} and locator: {locator}')
+            return None
 
 
