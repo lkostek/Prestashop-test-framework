4852be1ab85100c06eb28a68cab4b01aaa910a3d
Łukasz Kostek <lkostek@users.noreply.github.com>
2021-11-29 20:42:50 +0100

    refactored additional_functions.py to class;

--- utilities/additional_functions.py
+++ utilities/additional_functions.py
@@ -3,32 +3,37 @@ import random
 import logging
 from utilities.logger import loggerInstance
 
-log = loggerInstance(console_level=logging.DEBUG)
+# log = loggerInstance(console_level=logging.DEBUG)
 
-def generateRandomString(type_of_string, length):
-    """
-    Zwraca random string zlozony z wybranego typu znakow i dlugosci.
-    """
+class AdditionalFunctions:
 
-    generated_string = ''
-    type_dict = {
-        'lower_strings': string.ascii_lowercase,
-        'upper_strings': string.ascii_uppercase,
-        'digits': string.digits,
-        'digits_letters': string.digits + string.ascii_letters,
-    }
+    log = loggerInstance(console_level=logging.DEBUG)
 
-    generated_string = generated_string.join(
-        random.choice(type_dict['type_of_string']) for i in range(length)
-    )
+    def generateRandomString(self, type_of_string, length):
+        """
+        Zwraca random string zlozony z wybranego typu znakow i dlugosci.
+        """
 
-    log.info(f"Returning generated string: {generated_string}")
+        generated_string = ''
+        type_dict = {
+            'lower_strings': string.ascii_lowercase,
+            'upper_strings': string.ascii_uppercase,
+            'digits': string.digits,
+            'digits_letters': string.digits + string.ascii_letters,
+        }
 
-    return generated_string
+        generated_string = generated_string.join(
+            random.choice(type_dict[f'{type_of_string}']) for i in range(length)
+        )
 
-def getRandomString(type_of_string="digits_letters",length="6"):
-    """
-    Uzywa funkcji generateRandomString po to aby zwrocic random string
-    """
+        self.log.info(f"Returning generated string: {generated_string}")
 
-    return generateRandomString(type_of_string,length)
+        return generated_string
+
+    def getRandomString(self, type_of_string="digits_letters",length="6"):
+        """
+        Uzywa funkcji generateRandomString po to aby zwrocic random string
+        """
+
+        self.log.info("Generating string...")
+        return self.generateRandomString(type_of_string,length)
