ae0abea7e23eca268603b1a02a0d3d57d331289a
Łukasz Kostek <lkostek@users.noreply.github.com>
2021-12-03 10:07:02 +0100

    added second test test_addProductFromSearchToBasket; added third test test_removeProductsFromBasket;

--- tests/shopping/basket_tests.py
+++ tests/shopping/basket_tests.py
@@ -1,9 +1,11 @@
 import unittest
 
 import pytest
-from utilities.test_case_status import TestCaseStatus
+
 from pages.shopping.basket_page import BasketPage
 from utilities.additional_functions import AdditionalFunctions
+from utilities.test_case_status import TestCaseStatus
+
 
 @pytest.mark.usefixtures("setUpBeforeTest")
 class BasketTests(unittest.TestCase):
@@ -21,10 +23,10 @@ class BasketTests(unittest.TestCase):
         zostanie dodany do koszyka.
         """
 
-        self.basket_page.performAddProductFromHomePageToBasket() # wchodzi w produkt na stronie home
-        product_name_in_product_page = self.basket_page.getNameOfProductInProductPage() # pobiera nazwe produktu ze strony produktu
-        self.basket_page.performCheckProductInBasket() # wchodzi w koszyk
-        product_name_in_basket_page = self.basket_page.getNameOfProductHomeInBasket()
+        self.basket_page.performAddProductFromHomePageToBasket()
+        name_in_product_page = self.basket_page.getNameOfProductInProductPage()
+        self.basket_page.performCheckProductInBasket()
+        name_in_basket_page = self.basket_page.getNameOfProductHomeInBasket()
 
         test_result_1 = self.basket_page.checkBasketTitle(
             title='Koszyk',
@@ -35,27 +37,74 @@ class BasketTests(unittest.TestCase):
             test_result=test_result_1,
         )
 
-        test_result_2 = self.additional_functions.getResultOfCompareStrings( # porownanie nazwy produktu ze product page i basket page
-            string1=product_name_in_product_page,
-            string2=product_name_in_basket_page
+        test_result_2 = self.additional_functions.getResultOfCompareStrings(
+            string1=name_in_product_page,
+            string2=name_in_basket_page
         )
 
         self.test_status.setFinalTestResult(
             test_result=test_result_2,
             test_name="test_addProductFromHomeToBasket",
-            result_message="Product correctly added to basket.",
+            result_message="Product found in home page"
+                           " correctly added to basket.",
         )
 
+    @pytest.mark.run(order=2)
+    def test_addProductFromSearchToBasket(self):
+        """
+        Test sprawdza, czy po dodaniu produktu wyszukanego
+        z search zostanie dodany do koszyka.
+        """
 
-    # def test_addProductFromSearchToBasket(self):
-    #     """
-    #     Test sprawdza, czy po dodaniu produktu wyszukanego
-    #     z search zostanie dodany do koszyka.
-    #     """
-    #
-    # def test_removeProductsFromBasket(self):
-    #     """
-    #     Test sprawdza czy po usunieciu wszystkich
-    #     produktow z koszyka zostaly one usuniete.
-    #     """
+        self.basket_page.performAddProductFromSearchPageToBasket(
+            text='nike',
+        )
+        name_in_product_page = self.basket_page.getNameOfProductInProductPage()
+        self.basket_page.performCheckProductInBasket()
+        name_in_basket_page = self.basket_page.getNameOfProductNikeInBasket()
 
+        test_result_1 = self.basket_page.checkBasketTitle(
+            title='Koszyk',
+        )
+
+        self.test_status.setTestResult(
+            result_message="Titles are equals to each other.",
+            test_result=test_result_1,
+        )
+
+        test_result_2 = self.additional_functions.getResultOfCompareStrings(
+            string1=name_in_product_page,
+            string2=name_in_basket_page
+        )
+
+        self.test_status.setFinalTestResult(
+            test_result=test_result_2,
+            test_name="test_addProductFromSearchToBasket",
+            result_message="Product found from searchbox "
+                           "correctly added to basket.",
+        )
+
+    @pytest.mark.run(order=3)
+    def test_removeProductsFromBasket(self):
+        """
+        Test sprawdza czy po usunieciu wszystkich
+        produktow z koszyka zostaly one usuniete.
+        """
+
+        self.basket_page.performRemoveAllProductsFromBasket()
+
+        test_result_1 = self.basket_page.checkBasketTitle(
+            title='Koszyk',
+        )
+
+        self.test_status.setTestResult(
+            result_message="Titles are equals to each other.",
+            test_result=test_result_1,
+        )
+
+        test_result_2 = self.basket_page.isBacketEmpty()
+        self.test_status.setFinalTestResult(
+            test_result=test_result_2,
+            test_name="test_removeProductsFromBasket",
+            result_message="ALl products are removed from the basket.",
+        )
